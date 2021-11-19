88f31995e4877a3564c1cb55c6640c9ebd191a74
Carbon225 <carbon225@protonmail.com>
2021-11-19 21:01:31 +0100

    organize

--- src/poly.jl
+++ src/poly.jl
@@ -7,9 +7,8 @@ function main()
     ax = Axis(fig[2, 1], title="Interactive area")
     deregister_interaction!(ax, :rectanglezoom)
 
-    points = Node(Point2f[])
-    scatter!(ax, points)
 
+    points = Node(Point2f[])
     poly = @lift begin
         if length($points) < 3
             $points
@@ -21,34 +20,59 @@ function main()
     end
 
     lines!(ax, poly)
+    scatter!(ax, points)
+
+
+    function pushpoint!(p)
+        points[] = push!(points[], p)
+    end
+
+    function poppoint!()
+        length(points[]) > 0 && pop!(points[])
+        points[] = points[]
+    end
+
+    function clearpoints!()
+        points[] = empty(points[])
+    end
+
 
     mouseevents = addmouseevents!(ax.scene)
     onmouseleftdown(mouseevents) do event
         dpos = event.data
-        points[] = push!(points[], dpos)
+        pushpoint!(dpos)
     end
 
     on(events(fig).keyboardbutton) do event
         if event.action == Keyboard.press
             if event.key == Keyboard.r
-                points[] = empty(points[])
+                clearpoints!()
             elseif event.key == Keyboard.a
                 autolimits!(ax)
+            elseif event.key == Keyboard.p
+                poppoint!()
             end
         end
     end
+
 
     fig[1, 1] = controlsgrid = GridLayout(tellwidth=false)
     
     resetbtn = controlsgrid[1, 1] = Button(fig, label="Reset (R)")
     on(resetbtn.clicks) do n
-        points[] = empty(points[])
+        clearpoints!()
     end
 
     autoscalebtn = controlsgrid[1, 2] = Button(fig, label="Autoscale (A)")
     on(autoscalebtn.clicks) do n
         autolimits!(ax)
     end
+
+    popbtn = controlsgrid[1, 3] = Button(fig, label="Remove last (P)")
+    on(popbtn.clicks) do n
+        poppoint!()
+    end
+
 
     display(fig)
     nothing
